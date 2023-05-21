8a24d1e2e89b703ff5d1757ed6693f5f9f335d8f
Anastasija <127342966+ShaniThePug@users.noreply.github.com>
2023-05-21 15:49:52 +0300

    Update UserDaoHibernateImpl.java

--- PP_1_1_3-4_JDBC_Hibernate/src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ PP_1_1_3-4_JDBC_Hibernate/src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
-    Transaction transaction = null;
+    Transaction transaction;
 
     public UserDaoHibernateImpl() {
 
